3ccf9f16d9032a0234554e34dee14e21f2dbb001
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-04-13 10:46:17 +0700

    feat(editor): add insert link functionality

--- src/components/post/editor-navbar.tsx
+++ src/components/post/editor-navbar.tsx
@@ -25,58 +25,62 @@ export function EditorNavbar({ session }: { session: Session | null }) {
   const { isSaving } = useContext(EditorContext);
 
   return (
-    <nav className="container fixed top-0 z-10 w-full space-y-2 bg-primary-foreground py-2 drop-shadow-md backdrop-blur-md">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-5">
-          <div className="flex items-center gap-x-2">
-            <Image src={logo} alt="HIMARPL Logo" width={40} />
-            <Link
-              href="/me/posts/drafts"
-              className="hidden font-serif text-lg text-foreground/80 sm:inline"
-            >
-              <strong>Draft</strong> in @{session?.user.username}
-            </Link>
-          </div>
-          <div className="flex items-center gap-x-2">
-            {isSaving && (
-              <p className="font-serif text-lg text-foreground/80">Saving...</p>
-            )}
-            {!isSaving && (
-              <p className="font-serif text-lg text-foreground/80">Saved</p>
-            )}
-          </div>
-        </div>
-
-        <div className="ml-auto flex items-center gap-x-4">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage src={session?.user.image ?? ""} />
-                <AvatarFallback>
-                  {abbreviation(session?.user.name)}
-                </AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>{session?.user.name}</DropdownMenuLabel>
-              <DropdownMenuItem className="cursor-pointer">
-                <ExternalLink className="mr-2 h-4 w-4" />
-                My Profile
-              </DropdownMenuItem>
-
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={() => signOut()}
+    <>
+      <nav className="container fixed top-0 z-30 w-full space-y-2 bg-primary-foreground py-2 drop-shadow-md backdrop-blur-md">
+        <div className="z-50 flex items-center justify-between">
+          <div className="flex items-center gap-x-5">
+            <div className="flex items-center gap-x-2">
+              <Image src={logo} alt="HIMARPL Logo" width={40} />
+              <Link
+                href="/me/posts/drafts"
+                className="hidden font-serif text-lg text-foreground/80 sm:inline"
               >
-                <LogOut className="mr-2 h-4 w-4" />
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                <strong>Draft</strong> in @{session?.user.username}
+              </Link>
+            </div>
+            <div className="flex items-center gap-x-2">
+              {isSaving && (
+                <p className="font-serif text-lg text-foreground/80">
+                  Saving...
+                </p>
+              )}
+              {!isSaving && (
+                <p className="font-serif text-lg text-foreground/80">Saved</p>
+              )}
+            </div>
+          </div>
+
+          <div className="ml-auto flex items-center gap-x-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src={session?.user.image ?? ""} />
+                  <AvatarFallback>
+                    {abbreviation(session?.user.name)}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>{session?.user.name}</DropdownMenuLabel>
+                <DropdownMenuItem className="cursor-pointer">
+                  <ExternalLink className="mr-2 h-4 w-4" />
+                  My Profile
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() => signOut()}
+                >
+                  <LogOut className="mr-2 h-4 w-4" />
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
-      </div>
+      </nav>
 
       <EditorMenu />
-    </nav>
+    </>
   );
 }
