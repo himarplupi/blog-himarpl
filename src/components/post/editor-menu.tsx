b768a36e21d31301928c0c991ef3ab6791a03978
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-04-13 10:56:40 +0700

    fix(editor): menu zIndex onAnimationEnd

--- src/components/post/editor-menu.tsx
+++ src/components/post/editor-menu.tsx
@@ -79,6 +79,7 @@ export function EditorMenu() {
         className="container fixed top-14 z-20 w-full bg-primary-foreground pb-2"
         initial={{ y: -64 }}
         animate={{ y: 0 }}
+        onAnimationEnd={(e) => e.currentTarget.style.zIndex = "30"}
         transition={{ duration: 0.25, delay: 0.5 }}
       >
         <motion.div className="z-20 flex h-10 items-center gap-x-1 rounded-full bg-muted/50 px-3 py-1">
