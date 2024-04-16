f41e1e0346640d04ef8f24533ba609e83b48fb06
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-04-16 18:26:05 +0700

    feat(routes): postTag create route

--- src/server/api/routers/post-tag.ts
+++ src/server/api/routers/post-tag.ts
@@ -1,22 +1,45 @@
+import GithubSlugger from "github-slugger";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 
 export const postTagRouter = createTRPCRouter({
-  search: protectedProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    const inputs = input.split(" ");
-    return await ctx.db.postTag.findMany({
-      select: {
-        title: true,
-      },
-      where: {
-        OR: inputs.map((input) => ({
-          title: {
-            contains: input,
-            mode: "insensitive",
-          },
-        })),
-      },
-    });
-  }),
+  create: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const title = input;
+      const existingTag = await ctx.db.postTag.findFirst({
+        where: { title },
+      });
+
+      if (existingTag) throw new Error("Tag already exists");
+
+      const slugger = new GithubSlugger();
+      const slug = slugger.slug(title);
+
+      slugger.reset();
+
+      return await ctx.db.postTag.create({
+        data: { title, slug },
+      });
+    }),
+  search: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const inputs = input.split(" ");
+      return await ctx.db.postTag.findMany({
+        select: {
+          title: true,
+          id: true,
+        },
+        where: {
+          OR: inputs.map((input) => ({
+            title: {
+              contains: input,
+              mode: "insensitive",
+            },
+          })),
+        },
+      });
+    }),
 });
