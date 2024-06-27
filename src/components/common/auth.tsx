ae1d6149286d8ed2b331e046ea8541c588ea6261
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-06-27 10:51:00 +0700

    fix: open-graph images metadata

--- src/components/common/auth.tsx
+++ src/components/common/auth.tsx
@@ -23,7 +23,6 @@ import { SiGoogle } from "@icons-pack/react-simple-icons";
 export function GoogleLoginButton() {
   return (
     <Button variant="outline" onClick={() => signIn("google")}>
-      {/* @ts-expect-error React Simple Icons Lol Type */}
       <SiGoogle color="#fafafa" className="mr-2 h-4 w-4" />
       Login with Google
     </Button>
