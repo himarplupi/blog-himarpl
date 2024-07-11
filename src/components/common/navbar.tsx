bd7e879e5992744efd5799f15e7954ffeeb8f4e1
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-11 18:25:51 +0700

    feat(search): add input search in search result default page

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -3,21 +3,14 @@
 import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import type { Session } from "next-auth";
 import { signOut } from "next-auth/react";
-import {
-  Book,
-  CompassIcon,
-  LogOut,
-  Pen,
-  SearchIcon,
-  User,
-  XIcon,
-} from "lucide-react";
+import { Book, LogOut, Pen, SearchIcon, User } from "lucide-react";
 
+import { NavSearchInput } from "@/components/search/nav-search-input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -25,89 +18,10 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useSearchHistory } from "@/hooks/useSearchHistory";
 import logo from "@/images/logo.png";
 import { abbreviation, cn } from "@/lib/utils";
-import { Label } from "@radix-ui/react-label";
-
-import { Input } from "../ui/input";
 
 const hideNavbarOnRoutes = ["/login"];
-
-function NavSearchInput() {
-  const { searchHistory, removeFromSearchHistory } = useSearchHistory();
-  const [search, setSearch] = React.useState("");
-  const router = useRouter();
-  const shortedSearchHistory = searchHistory.slice(0, 3);
-
-  return (
-    <div className="group relative hidden md:flex">
-      <Label
-        htmlFor="search-input"
-        className={cn(
-          "absolute left-0 top-0 flex h-full w-10 items-center justify-center bg-transparent",
-        )}
-      >
-        <SearchIcon className="h-5 w-5" />
-      </Label>
-      <Input
-        type="search"
-        className="pl-10 md:min-w-80"
-        id="search-input"
-        placeholder="Cari"
-        autoComplete="off"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            router.push(`/search?q=${search}`);
-          }
-        }}
-      />
-
-      <ul className="absolute left-0 right-0 top-full z-50 mt-2 hidden overflow-hidden rounded-md border border-border bg-popover p-1 text-popover-foreground shadow-md duration-300 animate-in fade-in slide-in-from-top-5 group-focus-within:block">
-        {shortedSearchHistory.length > 0 && (
-          <div>
-            <p className="px-2 py-1.5 text-sm font-medium">Pencarian Terbaru</p>
-            {shortedSearchHistory.map((history) => (
-              <div className="flex items-center" key={history}>
-                <Link
-                  href={`/search?q=${history}`}
-                  onClick={() => {
-                    setSearch(history);
-                  }}
-                  className="flex h-10 basis-full items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                >
-                  <SearchIcon className="mr-2 h-5 w-5" />
-                  {history}
-                </Link>
-                <Button
-                  onClick={() => {
-                    removeFromSearchHistory(history);
-                  }}
-                  size="icon"
-                  variant="ghost"
-                  className="basis-auto"
-                >
-                  <XIcon className="h-5 w-5" />
-                </Button>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className="flex items-center">
-          <Link
-            href={"/explore-tags"}
-            className="flex h-10 basis-full items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-          >
-            <CompassIcon className="mr-2 h-5 w-5" />
-            Telusuri Label
-          </Link>
-        </div>
-      </ul>
-    </div>
-  );
-}
 
 export function Navbar({ session }: { session: Session | null }) {
   const pathname = usePathname();
--- src/components/search/search-result.tsx
+++ src/components/search/search-result.tsx
@@ -2,21 +2,25 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { MoreHorizontalIcon, XIcon } from "lucide-react";
+import { MoreHorizontalIcon, SearchIcon, XIcon } from "lucide-react";
 import { useQueryState } from "nuqs";
 
 import { Sidebar } from "@/components/home/sidebar";
 import { useSearchHistory } from "@/hooks/useSearchHistory";
 
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 
 import { SearchResultItems } from "./search-result-items";
 
 export function SearchResult() {
-  const [searchQuery] = useQueryState("q");
+  const [searchQuery, setSearchQuery] = useQueryState("q");
   const { searchHistory, addToSearchHistory, removeFromSearchHistory } =
     useSearchHistory();
+
+  const [search, setSearch] = React.useState("");
 
   React.useEffect(() => {
     if (searchQuery && searchQuery?.length > 0) {
@@ -30,33 +34,55 @@ export function SearchResult() {
     return (
       <div className="container">
         <ul className="max-w-screen-sm">
-          {searchHistory.length > 0 && (
-            <div>
-              <h2 className="pb-2 font-serif text-3xl font-bold italic tracking-wide first:mt-0">
-                Pencarian Terbaru
-              </h2>
-              {searchHistory.map((history) => (
-                <div className="flex items-center" key={history}>
-                  <Link
-                    href={`/search?q=${history}`}
-                    className="flex h-10 basis-full items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                  >
-                    {history}
-                  </Link>
-                  <Button
-                    onClick={() => {
-                      removeFromSearchHistory(history);
-                    }}
-                    size="icon"
-                    variant="ghost"
-                    className="basis-auto"
-                  >
-                    <XIcon className="h-5 w-5" />
-                  </Button>
-                </div>
-              ))}
-            </div>
-          )}
+          <div className="relative">
+            <Label
+              htmlFor="search"
+              className={
+                "absolute left-0 top-0 flex h-full w-10 items-center justify-center bg-transparent"
+              }
+            >
+              <SearchIcon className="h-5 w-5" />
+            </Label>
+            <Input
+              type="search"
+              className="pl-10 md:min-w-80"
+              id="search"
+              placeholder="Cari"
+              autoComplete="off"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyDown={async (e) => {
+                if (e.key === "Enter") {
+                  await setSearchQuery(search);
+                }
+              }}
+            />
+          </div>
+          <div className="mt-6">
+            <h2 className="pb-2 font-serif text-3xl font-bold italic tracking-wide first:mt-0">
+              Pencarian Terbaru
+            </h2>
+            {searchHistory.map((history) => (
+              <div className="flex items-center" key={history}>
+                <Link
+                  href={`/search?q=${history}`}
+                  className="flex h-10 basis-full items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                >
+                  {history}
+                </Link>
+                <Button
+                  onClick={() => {
+                    removeFromSearchHistory(history);
+                  }}
+                  size="icon"
+                  variant="ghost"
+                  className="basis-auto"
+                >
+                  <XIcon className="h-5 w-5" />
+                </Button>
+              </div>
+            ))}
+          </div>
         </ul>
       </div>
     );
