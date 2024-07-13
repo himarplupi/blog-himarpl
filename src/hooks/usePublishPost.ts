35f034750772de48eda1da0caff69d72c537adef
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-13 08:00:52 +0700

    fix: add delay in publishing for 2 seconds

--- src/hooks/usePublishPost.ts
+++ src/hooks/usePublishPost.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -8,6 +9,7 @@ import { api } from "@/trpc/react";
 export const usePublishPost = () => {
   const router = useRouter();
   const publishPost = api.post.publish.useMutation();
+  const [isPublishing, setIsPublishing] = React.useState(false);
 
   const publish = (id: string, title: string) => {
     const publishPromise = publishPost.mutateAsync({ id, title });
@@ -17,15 +19,22 @@ export const usePublishPost = () => {
       success: "Postingan selesai dipublikasi",
       error: "Postingan gagal dipublikasi",
     });
-
-    publishPromise
-      .then(() => {
-        router.push("/me/posts/public");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   };
 
-  return { publish, isPublishing: publishPost.isLoading };
+  React.useEffect(() => {
+    if (publishPost.isLoading) {
+      setIsPublishing(true);
+    } else if (publishPost.isSuccess) {
+      const id = setTimeout(() => {
+        router.push("/me/posts/public");
+        setIsPublishing(false);
+      }, 2000);
+
+      return () => clearTimeout(id);
+    }
+
+    //  eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [publishPost.isLoading]);
+
+  return { publish, isPublishing };
 };
