15b3e88f1b5f6046eddfef3caf20e67002af6666
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-11-24 09:52:29 +0700

    fix: lint error in lenis

--- src/components/common/lenis.tsx
+++ src/components/common/lenis.tsx
@@ -7,7 +7,7 @@ export function ReactLenis({ children }: { children: React.ReactNode }) {
   return (
     <RL
       options={{
-        prevent: false,
+        prevent: () => false,
       }}
       className="max-h-screen overflow-y-auto"
     >
